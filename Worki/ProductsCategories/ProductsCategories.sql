e13037e1ef1898d74dcf934fd9844d68b8c4479e
BrodovskiDenis <brodovski@region.ru>
2019-09-03 17:30:06 +0300

    Add database [ProductsCategories]

---
+++ Worki/ProductsCategories/ProductsCategories.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ProductsCategories]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ProductId] INT NOT NULL, 
+    [CategoryId] INT NOT NULL, 
+    CONSTRAINT [fkProductsCategories_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products]([Id]), 
+    CONSTRAINT [fkProductsCategories_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Categories]([Id])
+)
---
+++ Worki/ProductsCategories/uspProductsCategoriesAll.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[uspProductsCategoriesAll]
+AS
+BEGIN
+	SELECT
+		ProductName = P.Name,
+		CategoryName = C.Name
+	FROM Products as P
+		LEFT OUTER JOIN ProductsCategories as PC ON PC.ProductId = P.Id
+		LEFT OUTER JOIN Categories as C ON C.Id = PC.CategoryId
+END
