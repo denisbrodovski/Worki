022f04dc41880d42974fc0f8f6ee7fdf36d9e03d
Denis Brodovski <denbrod@gmail.com>
2019-09-03 21:56:38 +0300

    Add round to tests

--- Worki/Shape.Test/ShapeTest.cs
+++ Worki/Shape.Test/ShapeTest.cs
@@ -4,11 +4,11 @@ using Xunit;
 namespace Shape
 {
     public class ShapeTests
-    {
+    {   // поскольку площади фигур в большинстве дробные, то в тесте проверяем с точностью до сотых
         [Theory]
         [InlineData(-100, 0, typeof(ArgumentException))]
         [InlineData(0, 0, typeof(ArgumentException))]
-        [InlineData(10, 314, null)]
+        [InlineData(10, 314.16, null)]
         public void TestCircle(double R, double area, Type exceptionType)
         {
             try
@@ -16,7 +16,7 @@ namespace Shape
                 Circle circle = new Circle(R);
                 double result = circle.Area();
                 Assert.True(exceptionType == null, $"Area method is expected to throw {exceptionType}");
-                Assert.Equal(area, Math.Floor(result));
+                Assert.Equal(area, Math.Round(result, 2));
             }
             catch (Exception e)
             {
@@ -26,6 +26,7 @@ namespace Shape
 
         [Theory]
         [InlineData(1, 2, 3, 200, typeof(ArgumentException))]
+        [InlineData(6, 7, 9, 20.98, null)]
         public void TestTriangle(double a, double b, double c, double area, Type exceptionType)
         {
             try
@@ -33,7 +34,7 @@ namespace Shape
                 Triangle triangle = new Triangle(a, b, c);
                 double result = triangle.Area();
                 Assert.True(exceptionType == null, $"Area method is expected to throw {exceptionType}");
-                Assert.Equal(area, result);
+                Assert.Equal(area, Math.Round(result, 2));
             }
             catch (Exception e)
             {
